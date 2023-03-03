51af219e16424c011c200feb4eee181f8fe2b887
Anastasiia Lypai <lipaysha2012@gmail.com>
2023-03-03 13:07:27 +0100

    repaste the code from July 2022
    that code was submitted & checked by SheCodes. It works as it should work - API is responding, the weather showed is accurate

--- src/script.js
+++ src/script.js
@@ -1,53 +1,47 @@
-function getDate(now) {
- let currentDate = now.toLocaleString("en-US", {
-    hour12: false,
-  });
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-  let weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let weekDay = weekDays[now.getDay()];
-  //feb 17 2023 - adding day and month
-  let date = now.getDate();
-
-  return ${weekDay}, ${date} th, ${hour}:${minutes};
-}
-
 function searchForCity(event) {
-  let searchInput = document.querySelector("#search-bar-input").value;
+  let searchInput = document.querySelector("#search-bar-input");
   let city = document.querySelector("#submitted-city");
-  city.innerHTML = searchInput;
+  city.innerHTML = searchInput.value;
   let apiKey = "56d476ff22ec60814055fe5e814322ae";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCurrentWeatherConditions);
+  axios.get(url).then(showCurrentLocationTemperature);
 }
 
-function showCurrentWeatherConditions(response) {
+function showCurrentLocationTemperature(response) {
   let temp = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = temp;
 }
 
-function showPosition(response) {
-  let currentLocationButton = document.querySelector("#current-location-btn");
-  currentLocationButton.addEventListener("click", showPosition);
+// function showPosition(response) {
+//   let currentLocationButton = document.querySelector("#current-location-btn");
+//   currentLocationButton.addEventListener("click", showPosition);
 
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "56d476ff22ec60814055fe5e814322ae";
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCurrentWeatherConditions);
-}
+//   let latitude = position.coords.latitude;
+//   let longitude = position.coords.longitude;
+//   let apiKey = "56d476ff22ec60814055fe5e814322ae";
+//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+//   axios.get(url).then(showCurrentLocationTemperature);
+// }
 
-let today = document.querySelector("#date");
-let now = new Date();
-today.innerHTML = getDate(now);
+let currentDate = new Date();
+currentDate.toLocaleString("en-US", {
+  hour12: false
+});
+let hour = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let weekDay = weekDays[currentDate.getDay()];
+
+let date = document.querySelector("#date");
+date.innerHTML = `${weekDay}, ${hour}:${minutes}`;
 
 let temperatureElement = document.querySelector("#currentTemperature");
 let searchCityButton = document.querySelector("#search-btn");
