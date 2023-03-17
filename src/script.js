6f9f20922b444d109eaa4909c421b94da5356541
Anastasiia Lypai <lipaysha2012@gmail.com>
2023-03-17 13:12:21 +0100

    adding id to weather description

--- src/script.js
+++ src/script.js
@@ -1,27 +1,30 @@
+
 function searchForCity(event) {
   let searchInput = document.querySelector("#search-bar-input");
   let city = document.querySelector("#submitted-city");
   city.innerHTML = searchInput.value;
   let apiKey = "56d476ff22ec60814055fe5e814322ae";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCurrentLocationTemperature);
+  axios.get(url).then(showCurrentLocationWeather);
 }
 
-function showCurrentLocationTemperature(response) {
+function showCurrentLocationWeather(response) {
   let temp = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = temp;
+  let description = data.weather[0].description;
+  descriptionElement.innerHTML = description
 }
 
-function showPosition(response) {
-  let currentLocationButton = document.querySelector("#current-location-btn");
-  currentLocationButton.addEventListener("click", showPosition);
+// function showPosition(response) {
+//   let currentLocationButton = document.querySelector("#current-location-btn");
+//   currentLocationButton.addEventListener("click", showPosition);
 
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let apiKey = "56d476ff22ec60814055fe5e814322ae";
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCurrentLocationTemperature);
-}
+//   let latitude = position.coords.latitude;
+//   let longitude = position.coords.longitude;
+//   let apiKey = "56d476ff22ec60814055fe5e814322ae";
+//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+//   axios.get(url).then(showCurrentLocationWeather);
+// }
 
 let currentDate = new Date();
 currentDate.toLocaleString(
@@ -48,7 +51,69 @@ let date = document.querySelector("#date");
 date.innerHTML = `${weekDay}, ${hour}:${minutes}`;
 
 let temperatureElement = document.querySelector("#currentTemperature");
+let descriptionElement = document.querySelector("#weather-descrip");
 let searchCityButton = document.querySelector("#search-btn");
 searchCityButton.addEventListener("click", searchForCity);
 
 // navigator.geolocation.getCurrentPosition(showPosition);
+
+
+
+//  ATHENA code that is not working  05/03/2023
+// const apiKey = "56d476ff22ec60814055fe5e814322ae";
+// const searchInput = document.querySelector("#search-bar-input");
+// const city = document.querySelector("#submitted-city");
+// const temperatureElement = document.querySelector("#currentTemperature");
+// const descriptionElement = document.querySelector("#weather-descrip");
+// const searchCityButton = document.querySelector("#search-btn");
+// const date = document.querySelector("#date");
+
+// function setWeatherInfo(temp, description) {
+//   temperatureElement.innerHTML = Math.round(temp);
+//   descriptionElement.innerHTML = description;
+// }
+
+// async function showCurrentLocationWeather() {
+//   const url =
+//     "https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric";
+//   try {
+//     const response = await axios.get(url);
+//     const temp = response.data.main.temp;
+//     const description = response.data.weather[0].description;
+//     setWeatherInfo(temp, description);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// function setDate() {
+//   const now = new Date();
+//   const weekDay = now.toLocaleDateString("en-US", {weekday: "long" });
+//   const hour = now.getHours();
+//   const minutes = now.getMinutes().toString().padStart(2, "0");
+//   date.innerHTML = '${ weekDay }, ${ hour }:${ minutes }';
+//   let weekDays = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+//   let day = weekDays[currentDate.getDay()];
+// }
+
+// function searchForCity(event) {
+//   event.preventDefault();
+//   if (searchInput.value.trim() === "") {
+//     alert("Please enter a city name.");
+//     return;
+//   }
+//   city.innerHTML = searchInput.value;
+//   showCurrentLocationWeather();
+//   setDate();
+// }
+
+// searchCityButton.addEventListener("click", searchForCity);
+
